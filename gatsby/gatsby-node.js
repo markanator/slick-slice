3babf1147bf73f32db7a6ee6e818c3d5d7bb917f
Mark Ambro <wowmarkambro@gmail.com>
2020-09-25 15:45:01 -0500

    👌 IMPROVE: slicemasters page

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
   console.log('Turning the PIZZAS into PAGES!!!');
@@ -33,7 +34,7 @@ async function turnPizzasIntoPages({ graphql, actions }) {
 }
 
 async function turnToppingsIntoPages({ graphql, actions }) {
-  // console.log("Turning the toppings into pages!!!")
+  console.log('Turning the toppings into pages!!!');
   // get template
   const toppingTemplate = path.resolve('./src/pages/pizzas.js');
   // query all toppings
@@ -62,6 +63,42 @@ async function turnToppingsIntoPages({ graphql, actions }) {
   // pass topping data to pizza.js
 }
 
+// 3rd party api sourcing
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  // 1 fetch list of beers
+  const res = await fetch('https://sampleapis.com/beers/api/ale');
+  const beers = await res.json();
+  // 2 loop over each one
+  for (const beer of beers) {
+    // create a node for each beer
+    // const nodeContent = JSON.stringify(beer);
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    // create a node for that beer
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+  // console.log('🍺 Turn beers into nodes');
+}
+
+export async function sourceNodes(params) {
+  // fetch a list of bears and source them to gatsby
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
+}
 export async function createPages(params) {
   // create pages dynamically
   // concurrent promise waiting before moving along
--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,5 +1,71 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
 
-export default function BeersPage() {
-  return <>Im the beers page.</>;
+const BeerGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingeBeerStyles = styled.div`
+  border: 1px solid var(--gray);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+`;
+
+export default function BeersPage({ data: { beers } }) {
+  return (
+    <>
+      <h2 className="center">
+        We have {beers.nodes.length} beers available. Dine in Only!
+      </h2>
+
+      <BeerGridStyles>
+        {beers.nodes.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+          console.log(beer);
+          return (
+            <SingeBeerStyles key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              <p>{beer.price}</p>
+              <p title={`${rating} out of 5 stars`}>
+                {'⭐'.repeat(rating)}
+                <span style={{ filter: `grayscale(100%)` }}>
+                  {'⭐'.repeat(5 - rating)}
+                </span>
+                <span>({beer.rating.reviews})</span>
+              </p>
+            </SingeBeerStyles>
+          );
+        })}
+      </BeerGridStyles>
+    </>
+  );
 }
+
+export const query = graphql`
+  query BeerRun {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
